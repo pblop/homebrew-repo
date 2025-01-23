d7af5cbe10ab2d0025b68a34dbf4605d0ea3cbc6
pblop <pablo@pabl.eu>
2025-01-23 20:36:35 +0100

    fix(visual-paradigm@17.1): vp changed the urls, anything inside vp.com/downloads/vp17.1 just redirects to the windows download

--- Casks/visual-paradigm@17.1.rb
+++ Casks/visual-paradigm@17.1.rb
@@ -1,22 +1,14 @@
 cask "visual-paradigm@17.1" do
   arch arm: "AArch64", intel: "WithJRE"
 
-  version "17.1,20240501"
+  version "17.1"
   sha256 arm:   "0246f441c62a3c9486c96798f6e287e1a77d41ee83f471b57ae9adc5eb1bc91c",
          intel: "fcc3503007be435a5d7f1fdea8a0aa7dfba03972428ed613327b2dcb5bfb36ce"
 
-  url "https://www.visual-paradigm.com/downloads/vp#{version.csv.first}/#{version.csv.second}/Visual_Paradigm_#{version.csv.first.dots_to_underscores}_#{version.csv.second}_OSX_#{arch}.dmg"
+  url "https://www.visual-paradigm.com/downloads/vp#{version}/Visual_Paradigm_OSX_#{arch}.dmg"
   name "Visual Paradigm"
   desc "UML, SysML, BPMN modelling platform"
   homepage "https://www.visual-paradigm.com/"
-
-  livecheck do
-    url "https://www.visual-paradigm.com/downloads/vp/checksum.html"
-    regex(%r{/vp(\d+(?:\.\d+)+)/(\d+)/checksum\.html}i)
-    strategy :header_match do |headers, regex|
-      headers["location"].scan(regex).map { |match| "#{match[0]},#{match[1]}" }
-    end
-  end
 
   app "Visual Paradigm.app"
 
